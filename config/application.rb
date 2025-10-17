bb6c7beb8ddb94afdc2ea75b9c831b7677177cf5
Israel Vicars <israelvicars@gmail.com>
2025-10-17 01:53:30 -0400

    Fix: Add trace flags and disable master key requirement in entrypoint

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module GrantImporter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
+    
+    # Don't require master key - we use environment variables for secrets
+    config.require_master_key = false unless Rails.env.development?
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
